f16a09b61340bcfb4f232b7a00e990ef833a84b4
Thiago Marinho <thiagoo_100@hotmail.com>
2023-05-13 18:09:29 -0300

    final

--- app/src/main/java/com/example/debugging/MainActivity.kt
+++ app/src/main/java/com/example/debugging/MainActivity.kt
@@ -4,6 +4,9 @@ import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
+import org.w3c.dom.Text
+import kotlin.concurrent.thread
 
 private const val TAG = "MainActivity"
 
@@ -11,20 +14,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val helloTextView: TextView = findViewById(R.id.hello_world)
+        helloTextView.text = "Hello Debugging"
         logging()
         division()
     }
 
-    fun division(){
+    private fun division() {
         val numerator = 60
         var denominator = 4
-        repeat(5){
-            Log.v(TAG, "${numerator / denominator}")
-            denominator--
+        thread(start = true) {
+            repeat(4) {
+                Thread.sleep(3000)
+                runOnUiThread {
+                    findViewById<TextView>(R.id.divison_textview).setText("${numerator / denominator} ")
+                    denominator--
+                }
+            }
         }
+
     }
 
-    fun logging(){
+    fun logging() {
         Log.e(TAG, "ERROR: a serious error like an app crash")
         Log.w(TAG, "WARN: warns about the potential for serious errors")
         Log.i(TAG, "INFO: reporting technical information, such as an operation succeeding")
